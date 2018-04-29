927ce998789aa615245cbc5dba58251ec61da42b
reenavermaiiitm <reenavermaiiitm@gmail.com>
2018-04-29 12:59:00 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import  { Component } from 'react';
 import BookList from './containers/book-list';
+import BookDetail from './containers/book-detail';
 class App extends Component {
   render() {
     return (
     <div>
       <BookList />
+      <BookDetail />
     </div>
   );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import { combineReducers } from "redux";
 import BooksReducer from "./reducer_books";
-const rootReducer = combineReducers({
-  books: BooksReducer
+import ActiveBook from "./reducer_active_book";
 
+const rootReducer = combineReducers({
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
